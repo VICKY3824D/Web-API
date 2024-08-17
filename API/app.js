4b540f3f8f008875a353d5d2a3d44faec20119a3
vicky <vickypradana1111@gmail.com>
2024-08-17 16:32:54 +0700

    belajar axios

--- API/app.js
+++ API/app.js
@@ -53,24 +53,50 @@
 
 
 //Request Menggunakan async 
-const loadPeople = async () => {
-    try {
+// const loadPeople = async () => {
+//     try {
 
-        const res = await fetch('https://swapi.dev/api/people/3');
-        const data = await res.json();
-        console.log(data);
-        const res2 = await fetch('https://swapi.dev/api/people/5');
-        const data2 = await res2.json();
-        console.log(data2);
-        const res3 = await fetch('https://swapi.dev/api/people/9');
-        const data3 = await res3.json();
-        console.log(data3);
+//         const res = await fetch('https://swapi.dev/api/people/3');
+//         const data = await res.json();
+//         console.log(data);
+//         const res2 = await fetch('https://swapi.dev/api/people/5');
+//         const data2 = await res2.json();
+//         console.log(data2);
+//         const res3 = await fetch('https://swapi.dev/api/people/9');
+//         const data3 = await res3.json();
+//         console.log(data3);
         
-    } catch (err) {
-        console.log('error',err);
+//     } catch (err) {
+//         console.log('error',err);
+//     }
+// };
+
+// loadPeople();
+
+// axios
+//     .get('https://swapi.dev/api/people2/3')
+
+//     .then((res) =>{
+//         console.log(res.data);
+//     })
+
+//     .catch((err) => {
+//         console.log(err);
+//         alert(err.message);
+//     });
+
+const getPeople = async (id) => {
+    try {
+        const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+        console.log(res.data);
+    } catch (error) {
+        console.log(error);
+        console.log(error.message);
+        console.log(error.response.status);
+        console.log(error.response.data);
     }
-    
 };
 
-loadPeople();
-//wkwkwk
+for (let i = 1; i <= 10; i++) {
+    getPeople(i);
+}
