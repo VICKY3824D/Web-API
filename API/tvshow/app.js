4a6b296298b98e7c256f5e4d777ab94a353da11a
vidky <vickypradana1111@gmail.com>
2024-08-26 14:59:07 +0700

    menampilkan tvshow dalam document html

--- API/tvshow/app.js
+++ API/tvshow/app.js
@@ -1,11 +1,28 @@
 const form = document.querySelector('#search-form');
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
+form.addEventListener('submit', async (e) =>{
+    e.preventDefault(); //preventing refresh or default actions
+
+    //deleting tvshow image
+    document.querySelectorAll('img').forEach((img) => img.remove());
+
+    //get query value from input text
     const keyword = form.elements.query.value;
     const config = {
         params: { q: keyword},
     };
-    const res = axios.get('https://api.tvmaze.com/search/shows',config);
-    console.log(res.data);
+    const res = await axios.get('https://api.tvmaze.com/search/shows',config);
+    getImages(res.data);
+    form.elements.query.value = '';
 });
+
+const getImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            //add img source from image medium 
+            img.src = result.show.image.medium; 
+            document.body.append(img);
+        }
+    }
+};
